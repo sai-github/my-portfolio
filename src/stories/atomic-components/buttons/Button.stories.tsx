38466c72e5ad449969f0dc3a283a3bd20649846f
sai-github <saiediitm@gmail.com>
2023-06-28 19:54:10 +0530

    feat: create button and story

---
+++ src/stories/atomic-components/buttons/Button.stories.tsx
@@ -0,0 +1,55 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+
+import Button from "./Button";
+import { Send } from "iconsax-react";
+
+const meta = {
+  title: "Example/Button",
+  component: Button,
+  tags: ["autodocs"],
+  argTypes: {
+    variant: {
+      options: ["primary", "secondary", "outline"],
+      control: { type: "select" },
+    },
+    onClick: { action: "clicked" },
+  },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    variant: "primary",
+    children: "Button",
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    variant: "secondary",
+    children: "Button",
+  },
+};
+
+export const Outline: Story = {
+  args: {
+    variant: "outline",
+    children: "Button",
+  },
+};
+
+export const WithIconPrimary: Story = {
+  render: () => (
+    <Button icon={Send} onClick={action("clicked")}>
+      Hello
+    </Button>
+  ),
+  name: "Button with Icon",
+};
+
+export const IconButton: Story = {
+  render: () => <Button icon={Send} onClick={action("clicked")} />,
+};
---
+++ src/stories/atomic-components/buttons/Button.tsx
@@ -0,0 +1,33 @@
+import clsx from "clsx";
+import "./button.css";
+import { Icon } from "iconsax-react";
+
+interface ButtonProps {
+  variant?: "primary" | "secondary" | "outline";
+  icon?: Icon;
+  children?: React.ReactNode;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+const Button = ({
+  variant = "primary",
+  icon: Icon,
+  children,
+  onClick,
+}: ButtonProps) => {
+  const btnCls = clsx({
+    "btn-primary": variant === "primary",
+    "btn-secondary": variant === "secondary",
+    "btn-outline": variant === "outline",
+    "px-2": !children, // change padding when no children
+  });
+
+  return (
+    <button className={btnCls} onClick={onClick}>
+      {Icon && <Icon size="2rem" color="var(--current-text)" variant="Bulk" />}
+      {children}
+    </button>
+  );
+};
+
+export default Button;
