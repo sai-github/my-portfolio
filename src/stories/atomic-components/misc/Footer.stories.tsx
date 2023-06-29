64ccebc7c0932815e4de3df3f6ae1e40de0b7d39
sai-github <saiediitm@gmail.com>
2023-06-29 19:22:20 +0530

    feat: create footer and story

---
+++ src/stories/atomic-components/misc/Footer.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Footer from "./Footer";
+
+const meta = {
+  title: "Example/Footer",
+  component: Footer,
+  tags: ["autodocs"],
+} satisfies Meta<typeof Footer>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const DesktopFooter: Story = {};
+
+export const MobileFooter: Story = {
+  parameters: {
+    viewport: {
+      defaultViewport: "iphone6",
+    },
+  },
+};
---
+++ src/stories/atomic-components/misc/Footer.tsx
@@ -0,0 +1,45 @@
+import clsx from "clsx";
+import useIsDesktop from "../../../utils/hooks/useIsDesktop";
+import "./footer.css";
+
+const CC_LOGO = {
+  desktop: "https://licensebuttons.net/l/by/4.0/88x31.png",
+  mobile: "https://licensebuttons.net/l/by/4.0/80x15.png",
+};
+const Footer = () => {
+  const isDesktop = useIsDesktop();
+  const textCls = clsx(isDesktop ? "subtitle" : "caption"); // careful with color overrides from class as well
+
+  return (
+    <div className="footer">
+      <a
+        rel="license noopener noreferrer"
+        href="http://creativecommons.org/licenses/by/4.0/"
+        target="_blank"
+      >
+        <img
+          className="cc"
+          src={CC_LOGO[isDesktop ? "desktop" : "mobile"]}
+          alt="Creative Commons License"
+        />
+      </a>
+
+      <span className={textCls}>
+        Designed with{" "}
+        <a
+          href="https://spline.design/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Spline
+        </a>{" "}
+        and{" "}
+        <a href="https://figma.com/" target="_blank" rel="noopener noreferrer">
+          Figma
+        </a>
+      </span>
+    </div>
+  );
+};
+
+export default Footer;
