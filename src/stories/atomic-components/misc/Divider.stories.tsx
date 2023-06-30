4785d3662a1b31629c8ebcf17b1dbb6619b48833
sai-github <saiediitm@gmail.com>
2023-06-30 20:16:52 +0530

    feat: create divider and story

---
+++ src/stories/atomic-components/misc/Divider.stories.tsx
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Divider from "./Divider";
+const meta = {
+  title: "Example/Divider",
+  component: Divider,
+  tags: ["autodocs"],
+  argTypes: {
+    dir: { options: ["hz", "vt"], control: { type: "select" } },
+    breakText: { control: "text" },
+  },
+  args: {
+    dir: "hz",
+    breakText: "OR",
+  },
+} satisfies Meta<typeof Divider>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Horizontal: Story = {
+  args: {
+    dir: "hz",
+  },
+};
+
+export const Vertical: Story = {
+  args: {
+    dir: "vt",
+  },
+
+  render: (args) => (
+    <div style={{ height: 400, width: "100%" }}>
+      <Divider {...args} />
+    </div>
+  ),
+};
---
+++ src/stories/atomic-components/misc/Divider.tsx
@@ -0,0 +1,26 @@
+import clsx from "clsx";
+import "./divider.css";
+
+interface DividerProps {
+  dir: "hz" | "vt";
+  breakText: string;
+}
+
+const Divider = ({ dir, breakText }: DividerProps) => {
+  const containerCls = clsx("divider-container", {
+    "flex-row": dir === "hz",
+    "flex-col": dir === "vt",
+    "h-[100%]": dir === "vt",
+  });
+  const lineCls = clsx("line", dir === "hz" ? "h-[2px]" : "w-[2px]");
+
+  return (
+    <div className={containerCls}>
+      <div className={lineCls}></div>
+      <div className="label small">{breakText}</div>
+      <div className={lineCls}></div>
+    </div>
+  );
+};
+
+export default Divider;
