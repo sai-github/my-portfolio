92ec11462d574a4dfe5e2e7f2f60a7da00d0a493
sai-github <saiediitm@gmail.com>
2023-07-02 03:57:19 +0530

    fix: group light and dark colors

--- src/stories/intro/Colors.stories.tsx
+++ src/stories/intro/Colors.stories.tsx
@@ -30,17 +30,25 @@ const ColorBox = ({ color, name }: { color: string; name: string }) => (
 );
 
 export const AllColors = () => (
-  <div style={{ display: "flex", flexWrap: "wrap" }}>
-    <ColorBox color="var(--light-background)" name="Light Background" />
-    <ColorBox color="var(--light-text)" name="Light Text" />
-    <ColorBox color="var(--light-primary)" name="Light Primary" />
-    <ColorBox color="var(--light-secondary)" name="Light Secondary" />
-    <ColorBox color="var(--light-accent)" name="Light Accent" />
-    <ColorBox color="var(--dark-background)" name="Dark Background" />
-    <ColorBox color="var(--dark-text)" name="Dark Text" />
-    <ColorBox color="var(--dark-primary)" name="Dark Primary" />
-    <ColorBox color="var(--dark-secondary)" name="Dark Secondary" />
-    <ColorBox color="var(--dark-accent)" name="Dark Accent" />
+  <div
+    style={{ display: "flex", flexDirection: "column", alignItems: "start" }}
+  >
+    <h6>Light Theme</h6>
+    <div style={{ display: "flex", flexWrap: "wrap" }}>
+      <ColorBox color="var(--light-background)" name="Light Background" />
+      <ColorBox color="var(--light-text)" name="Light Text" />
+      <ColorBox color="var(--light-primary)" name="Light Primary" />
+      <ColorBox color="var(--light-secondary)" name="Light Secondary" />
+      <ColorBox color="var(--light-accent)" name="Light Accent" />
+    </div>
+    <h6 style={{ marginTop: "20px" }}>Dark Theme</h6>
+    <div style={{ display: "flex", flexWrap: "wrap" }}>
+      <ColorBox color="var(--dark-background)" name="Dark Background" />
+      <ColorBox color="var(--dark-text)" name="Dark Text" />
+      <ColorBox color="var(--dark-primary)" name="Dark Primary" />
+      <ColorBox color="var(--dark-secondary)" name="Dark Secondary" />
+      <ColorBox color="var(--dark-accent)" name="Dark Accent" />
+    </div>
     {/* Add more colors as needed... */}
   </div>
 );
