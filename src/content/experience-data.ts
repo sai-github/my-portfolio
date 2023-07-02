fc4d815c52771ca861f06f0f42bf55f76eb25326
sai-github <saiediitm@gmail.com>
2023-07-02 16:15:38 +0530

    api: add content as local data

---
+++ src/content/experience-data.ts
@@ -0,0 +1,40 @@
+import { IExperience } from "./models";
+
+export const experienceList: IExperience[] = [
+  {
+    logoLight: "https://dummyimage.com/64x64/8f8f8f/0d0d0d&text=logo",
+    logoDark: "https://dummyimage.com/64x64/1a1a1a/f2f2f2&text=logo",
+    title: "Engineer",
+    timeline: "Sept '18 - Oct '23",
+    company: "Airbus",
+    data: {
+      type: "quote",
+      quote:
+        "Refactored React codebase and implemented core features for a tool which provides statistical and analytical capabilities for Material quality and Test reduction",
+    },
+  },
+  {
+    logoLight: "https://dummyimage.com/64x64/8f8f8f/0d0d0d&text=logo",
+    logoDark: "https://dummyimage.com/64x64/1a1a1a/f2f2f2&text=logo",
+    title: "Engineer",
+    timeline: "Sept '18 - Oct '23",
+    company: "Airbus",
+    data: {
+      type: "quote",
+      quote:
+        "Refactored React codebase and implemented core features for a tool which provides statistical and analytical capabilities for Material quality and Test reduction",
+    },
+  },
+  {
+    logoLight: "https://dummyimage.com/64x64/8f8f8f/0d0d0d&text=logo",
+    logoDark: "https://dummyimage.com/64x64/1a1a1a/f2f2f2&text=logo",
+    title: "Engineer",
+    timeline: "Sept '18 - Oct '23",
+    company: "Airbus",
+    data: {
+      type: "quote",
+      quote:
+        "Refactored React codebase and implemented core features for a tool which provides statistical and analytical capabilities for Material quality and Test reduction",
+    },
+  },
+];
---
+++ src/content/models/index.ts
@@ -0,0 +1,18 @@
+type IExperienceData =
+  | {
+      type: "quote";
+      quote: string;
+    }
+  | {
+      type: "list";
+      list: string[];
+    };
+
+export interface IExperience {
+  logoLight: string;
+  logoDark?: string;
+  title: string;
+  timeline: string;
+  company: string;
+  data: IExperienceData;
+}
